de9dbdf6c86fdf67902471cc6ebb697c3bcdf0db
corenbla <corenbla@gmail.com>
2021-02-15 14:58:29 +0100

    onClick callback event + Toast

--- app/src/main/java/com/example/itakademy_android/MainActivity.java
+++ app/src/main/java/com/example/itakademy_android/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.itakademy_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.title);
+        textView.setText(R.string.app_name);
+    }
+
+    public void foo(View view) {
+        Button leftButton = findViewById(R.id.homeButton1);
+        Button rightButton = findViewById(R.id.homeButton2);
+
+        leftButton.setText(R.string.tada);
+        rightButton.setText(R.string.tada);
+
+         Toast.makeText(this, "🎊", Toast.LENGTH_SHORT).show();
     }
 }
